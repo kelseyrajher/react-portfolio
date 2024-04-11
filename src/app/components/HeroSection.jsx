08ab2ca77bbd809c7b1f0928ac83ec1ed3c9b721
Kelsey Rajher <kelseyrajher@Kelseys-MacBook-Pro-2.local>
2024-04-11 15:46:13 -0400

    Edits to HeroSection

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -8,7 +8,7 @@ import Link from 'next/link';
 
 export const HeroSection = () => {
     return (
-        <section className='lg:py-16'>
+        <section className="lg:py-16">
             <div className="grid grid-cols-1 lg:grid-cols-12">
                 <motion.div
                     initial={{ opacity: 0, scale: 0.5 }}
@@ -16,13 +16,13 @@ export const HeroSection = () => {
                     transition={{ duration: 0.5 }}
                     className="col-span-8 place-self-center text-center sm:text-left justify-self-start">
                     <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-7xl lg:leading-normal font-extrabold">
-                        <span className='text-transparent bg-clip-text bg-gradient-to-r from-primary-400 to-secondary-600'>Hello, I'm </span>
+                        <span className="text-transparent bg-clip-text bg-gradient-to-r from-primary-400 to-secondary-600">Hello, I'm </span>
                         <br></br>
                         <TypeAnimation
                             sequence={[
-                                'Kelsey',
+                                "Kelsey",
                                 1000,
-                                'Web Developer',
+                                "Web Developer",
                                 1000
                             ]}
                             wrapper="span"
@@ -30,12 +30,12 @@ export const HeroSection = () => {
                             repeat={Infinity}
                         />
                     </h1>
-                    <p className='text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl'>I am passionate about crafting visually stunning and user-friendly experiences on the web.
+                    <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">I am passionate about crafting visually stunning and user-friendly experiences on the web.
                     </p>
                     <div>
-                        <a href="#contact"><button className='px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br  from-primary-400 to-secondary-600 hover:bg-slate-200 text-white'>Get in Touch</button></a>
-                        <a href="#projects"><button className='px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br  from-primary-400 to-secondary-600 hover:bg-slate-800 text-white mt-3'>
-                            <span className='block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2'>View my Work</span>
+                        <a href="#contact"><button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br  from-primary-400 to-secondary-600 hover:bg-slate-200 text-white">Get in Touch</button></a>
+                        <a href="#projects"><button className="px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br  from-primary-400 to-secondary-600 hover:bg-slate-800 text-white mt-3">
+                            <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">View my Work</span>
                         </button></a>
                     </div>
                 </motion.div>
@@ -43,11 +43,11 @@ export const HeroSection = () => {
                     initial={{ opacity: 0, scale: 0.5 }}
                     animate={{ opacity: 1, scale: 1 }}
                     transition={{ duration: 0.5 }}
-                    className='col-span-4 place-self-center mt-4 lg:mt-0'>
+                    className="col-span-4 place-self-center mt-4 lg:mt-0">
                     <Image
                         src="/images/headshot-good.png"
                         alt="hero image"
-                        className='rounded-full'
+                        className="rounded-full"
                         width={300}
                         height={300}
                     />

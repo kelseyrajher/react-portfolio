6f7cbabe0f26f174d11e1c07d9d82b09a7a0ed5c
Kelsey Rajher <kelseyrajher@Kelseys-MacBook-Pro-2.local>
2024-04-11 15:51:22 -0400

    HeroSection edits

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -16,7 +16,7 @@ export const HeroSection = () => {
                     transition={{ duration: 0.5 }}
                     className="col-span-8 place-self-center text-center sm:text-left justify-self-start">
                     <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-7xl lg:leading-normal font-extrabold">
-                        <span className="text-transparent bg-clip-text bg-gradient-to-r from-primary-400 to-secondary-600">Hello, I'm </span>
+                        <span className="text-transparent bg-clip-text bg-gradient-to-r from-primary-400 to-secondary-600">Hello&lsquo; I&apos;m </span>
                         <br></br>
                         <TypeAnimation
                             sequence={[
